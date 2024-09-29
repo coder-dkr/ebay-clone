44077a4ebbcbbfa27fb1a11a7246798a87d84aa8
Dhruv Roy <dhruvakr9001@gmail.com>
2024-09-29 19:16:30 +0530

    style fixes

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -6,7 +6,7 @@ const HeroSection = () => {
   return (
     <>
       <Carousel />
-      <Offer bg="#71e3e2" color="rgb(0_49_71)" title="Up to 60% off select Apple tech" sub="Save on iPhones, MacBooks, and more with a one-year warranty." btn="Show now" />
+      <Offer bg="#71e3e2" color="#003147" title="Up to 60% off select Apple tech" sub="Save on iPhones, MacBooks, and more with a one-year warranty." btn="Show now" />
     </>
   )
 }
